6a207284d755764f7024c952f9f4f83b6cbddff5
nagibotr1410 <nagibotr1410@gmail.com>
2025-12-10 17:38:30 +1100

    last

---
+++ Assets/Bicycle2D/Scripts/Finish.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Finish : MonoBehaviour
+{
+    public UnityEvent OnFinished;
+    private bool riderCheck = false;
+    private bool bikeCheck = false;
+
+}
